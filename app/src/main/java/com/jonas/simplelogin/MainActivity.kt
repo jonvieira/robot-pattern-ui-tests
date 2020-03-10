196716f64b35b893265beaacfa0d7d4d9aa3e1ce
Jonas Vieira <jonasvieira.ti@gmail.com>
2020-03-10 12:53:25 -0300

    feat(project): initial commit

---
+++ app/src/main/java/com/jonas/simplelogin/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.jonas.simplelogin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
